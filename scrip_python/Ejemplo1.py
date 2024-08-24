e7b9b25dc2d9f3b41c079778ae40ee91962839e3
Estudiante <rossyvalencia12@gmail.com>
2024-08-23 21:02:09 -0500

    Script python 1

---
+++ scrip_python/Ejemplo1.py
@@ -0,0 +1,13 @@
+#ESTE ES UN COMENTARIO DE LINEA
+'''
+Esto es un comentario de bloque
+'''
+
+n1 = 10
+n2 = 20
+suma = n1 + n2
+print (suma)
+print("La suma es: ",suma)
+print(f"La suma es: {suma}")
+print ("La suma es: ",n1 + n2)
+
