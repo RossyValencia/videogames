7696fcd6115c7627f3abe09a1e918e45b0d1c8bf
Estudiante <rossyvalencia12@gmail.com>
2024-09-13 21:37:33 -0500

    message

---
+++ scrip_python/number_race/main.py
@@ -0,0 +1,13 @@
+'''Script description: Number Race
+Dev: Rossy Valencia
+Date: 1309/2024
+
+'''
+from random import randint
+
+
+dice1 = randint(1,6)
+dice2 = randint(1,6)
+
+print(f"Dice 1; {dice1}")
+print(f"Dice 2; {dice2}")
